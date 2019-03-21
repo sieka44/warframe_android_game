40d997634c2876c6da7daf46c7fef84488361b6d
sieka44 <sieka444@interia.pl>
2019-03-21 08:37:58 +0100

    initial commit

---
+++ Assets/Scripts/GodotEnemyScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GodotEnemyScript : MonoBehaviour {
+
+    Rigidbody2D rigidBody2d;
+    Text label;
+    int health;
+	// Use this for initialization
+	void Start ()
+    {
+        health = 100;
+        rigidBody2d = GetComponent<Rigidbody2D>();
+        label = transform.Find("Canvas").Find("Text").GetComponent<Text>();
+        rigidBody2d.velocity = new Vector2(2, 13);
+        rigidBody2d.angularVelocity = 3;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        health -= collision.gameObject.GetComponent<SaberScript>().getDamage();
+        Debug.Log(health);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (health <= 0) Destroy(gameObject);
+        label.text = "Health: " + health + "%";
+        
+	}
+}
---
+++ Assets/Scripts/SaberScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaberScript : MonoBehaviour
+{
+
+    private Vector3 defaultLocation;
+    private int damage = 25; 
+    // Use this for initialization
+    void Start ()
+    {
+        defaultLocation.Set(-10, 0, 0);
+	}
+
+    public int getDamage()
+    {
+        return damage;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            newPosition.z = 0;
+            transform.position = newPosition;
+        }
+        else
+        {
+            transform.position = defaultLocation;
+        }
+    }
+}
+    
